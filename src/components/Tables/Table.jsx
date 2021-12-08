2c8bafbec751a4e3be6a26d7d99f507ad7bc2960
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-12-08 08:55:08 +0100

    Created the table component

--- src/components/Tables/Table.jsx
+++ src/components/Tables/Table.jsx
@@ -7,7 +7,7 @@ function Table({ title, head, data, column, paginate }) {
     return (
         <div className="bg-white border border-gray-200 rounded-sm shadow-lg col-span-full xl:col-span-8">
             <header className="px-5 py-4 border-b border-gray-100">
-                <h2 className="font-semibold text-gray-800">{title}</h2>
+                <h2 className="font-semibold text-gray-800">{title} {!title && (<span>No Title</span>)}</h2>
             </header>
             <div className="p-3">
 
