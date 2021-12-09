00dc7efe729803dc15337dbe79483c33de762acf
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-12-09 10:02:21 +0100

    Fixed design style ogf the authentication

--- src/components/Form/SelectOption.jsx
+++ src/components/Form/SelectOption.jsx
@@ -1,6 +1,6 @@
 const SelectOption = ({ label, name, error, options, ...props }) => {
     return (
-        <div className="w-full my-5">
+        <div className="w-full mb-5">
             <label htmlFor={name} className="text-sm font-bold tracking-wide text-gray-900">{label}</label>
             <select name={name} {...props} className="w-full overflow-hidden font-medium border-b border-purple-500 hover:border-b-2 h-11 focus:outline-none">
                 <option value="" disabled selected>Select</option>
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
                 <InputPasswordField name="password" type="password" placeholder="Password" label="Password" />
                 <SelectOption name="type" options={['Manager', 'Admin', 'User']} label="Login As" />
                 <div className="flex items-center justify-between w-full">
-                    <Link to="/auth/forget-password" className="text-sm text-purple-500 hover:text-purple-600" >Forgot password?</Link>
+                    <Link to="/auth/forget-password" className="text-sm font-semibold tracking-wide text-purple-500 hover:text-purple-600" >Forgot password?</Link>
                     <button className="px-4 py-2 font-bold text-white bg-purple-600 hover:bg-purple-700">Login</button>
                 </div>
             </form>
