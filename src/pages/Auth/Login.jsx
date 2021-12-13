822d3a4128e8a88a1b57b5c8ee85b2d3556d986d
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-12-13 16:55:05 +0100

    Added useForm hooks

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -5,19 +5,26 @@ import SelectOption from './../../components/Form/SelectOption';
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from './../../services/AuthSlice';
 import { FaCircleNotch } from "react-icons/fa";
+import useForm from "../../hooks/useForm";
 
 const Login = () => {
 
     const dispatch = useDispatch();
     const { loading, error } = useSelector(state => state.auth);
 
+    const initialState = { email: '', password: '', type: 'select' };
+    const [values, setValues] = useForm(initialState);
+
+
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        console.log(values)
         dispatch(loginUser({
-            path: "admin",
+            path: values.type,
             payload: {
-                email: 'abdullahij951@gmail.com',
-                password: "password",
+                email: values.email,
+                password: values.password,
             }
         }))
     }
@@ -26,9 +33,9 @@ const Login = () => {
         <div className="w-full h-full">
             <h2 className="text-2xl font-bold text-center text-purple-600 text-opacity-70">Login</h2>
             <form onSubmit={handleSubmit} className="flex flex-col items-start justify-start w-full">
-                <InputField name="email" type="email" placeholder="Email" label="Email" error={error?.email} />
-                <InputPasswordField name="password" type="password" placeholder="Password" label="Password" error={error?.password} />
-                <SelectOption name="type" options={['Manager', 'Admin', 'User']} label="Login As" />
+                <InputField name="email" type="email" placeholder="Email" label="Email" error={error?.email} onChange={setValues} />
+                <InputPasswordField name="password" type="password" placeholder="Password" label="Password" error={error?.password} onChange={setValues} />
+                <SelectOption name="type" options={['Manager', 'Admin', 'User']} label="Login As" onChange={setValues} />
                 <div className="flex items-center justify-between w-full">
                     <Link to="/auth/forget-password" className="text-sm font-semibold tracking-wide text-purple-500 hover:text-purple-600" >Forgot password?</Link>
                     <button className={`py-2 font-bold text-white px-4 disable:bg-purple-300 bg-purple-600 hover:bg-purple-700 disabled:hover:bg-purple-300 shadow-md shadow-purple-400 disabled:shadow-slate-200 rounded`} disabled={loading}>
